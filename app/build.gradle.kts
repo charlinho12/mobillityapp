84adb1d08d336da56181ddc7095a419f564820b2
Seu Nome <seu@email.com>
2023-12-11 13:34:08 -0300

    commitprojeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.1.2")
     implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.22")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/fatec/telalogin/MainActivity.java
+++ app/src/main/java/com/fatec/telalogin/MainActivity.java
@@ -3,12 +3,22 @@ package com.fatec.telalogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView EdtMapa;
+    private ImageView ImgMapaBsb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImgMapaBsb = findViewById(R.id.ImgMapaBsb);
+        EdtMapa = findViewById(R.id.EdtMapa);
+
     }
 }
